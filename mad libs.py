a722eaf1ee50c60f4530e3cd7f6df953eacde382
Iwantdiesmh <49891680+Iwantdiesmh@users.noreply.github.com>
2020-11-25 08:18:40 +0800

    Add files via upload

---
+++ mad libs.py
@@ -0,0 +1,9 @@
+propernoun = input('name a person [proper noun]: ')
+noun = input('name an object [noun]: ')
+verb = input('name a verb that includes -ing[verb]: ')
+adjective = input('name an adjective [adjective]: ')
+verbtwo = input('name another verb that includes -ing[verb]: ')
+adjectivetwo = input('name a different adjective [adjective]: ')
+verbthird = input('name a third verb that includes -ed[verb]: ')
+
+print("once upon a time,", propernoun, "was", verb, "up a hill when he met a small", noun, "that was", verbtwo, adjectivetwo + ". He", verbthird, "it and ran off. the end.")
