f96d08b5358e5f086a2876baab524e51cf9e16bf
Iwantdiesmh <49891680+Iwantdiesmh@users.noreply.github.com>
2020-12-09 08:04:22 +0800

    Delete converter.py

--- converter.py
+++
@@ -1,9 +0,0 @@
-cm = int(input('enter centimeters to convert to inches: '))
-inches = cm/2.54
-print(cm, 'cm is', inches, 'in')
-kg = int(input('enter kilograms to convert to pounds: '))
-pounds = kg * 2.20462
-print(kg, 'kilograms is', pounds, 'lbs')
-c = int(input('enter celcius to convert to fahrenheit: '))
-fahrenheit = c * 9/5 + 32
-print(c, 'C is', fahrenheit, 'F')
